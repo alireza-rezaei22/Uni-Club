e6a9d8df08aa75f85470fde014d23f5d978b61f9
alireza <ar.rezaei124@gmail.com>
2026-05-24 18:52:53 +0330

    style(mobile): fix mobile layout in ostadInfo

--- src/app/ostad/[ostadInfo]/page.jsx
+++ src/app/ostad/[ostadInfo]/page.jsx
@@ -34,14 +34,14 @@ export default async function Ostad({ params }) {
     if (token) {
         userInfo = verify(token, process.env.ACCESSTOKEN_SECRETKEY)
     }
-    
+
     return (
         <>
             <div
                 className='p-5 mb-12 space-y-2 text-zinc-200
                     md:flex flex-col gap-5 md:mx-[5%]
                 '>
-                <div className='flex gap-10 items-center justify-center'>
+                <div className='flex flex-col md:gap-10 md:flex-row md:items-center justify-center'>
                     <Image
                         className='w-48 max-w-64 h-48 max-h-64 m-auto rounded-full'
                         src={image || "/images/defaultPerson.png"}
@@ -49,17 +49,17 @@ export default async function Ostad({ params }) {
                         width={300}
                         height={300}
                     />
-                    <span className='self-end flex-1 flex flex-col  justify-between items-start my-5 gap-1 '>
-                        <h2 className='text-lg md:text-2xl font-bold md:mb-5'>استاد {name}</h2>
-                        <span className='flex gap-2'>
+                    <span className='md:self-end flex-1 flex flex-col  justify-between items-start my-5 gap-1 '>
+                        <h2 className='text-2xl font-bold md:mb-5'>استاد {name}</h2>
+                        <span className='flex gap-2 items-center'>
                             <GraduationCapIcon />
-                            <h4 className='font-medium'>مدرک: {ostadDegree}</h4>
+                            <h4 className='font-medium'>مدرک تحصیلی: {ostadDegree}</h4>
                         </span>
-                        <span className='flex gap-2'>
+                        <span className='flex gap-2 items-center'>
                             <GraduationCapIcon />
                             <h4 className='font-medium'>رشته تحصیلی: {studyField || 'مشخص نشده'}</h4>
                         </span>
-                        <span className='flex gap-2'>
+                        <span className='flex gap-2 items-center'>
                             <GraduationCapIcon />
                             <h4 className='font-medium'>نوع دروس: {ostadcategory}</h4>
                         </span>
@@ -67,14 +67,14 @@ export default async function Ostad({ params }) {
 
                         <span className='w-full flex justify-between items-center gap-2'>
 
-                            <span className='flex gap-2'>
+                            <span className='flex gap-2 items-center'>
                                 <PenToolIcon />
                                 <h4 className='font-medium'>تاریخ شروع فعالیت: {startYear == '-1' ? 'مشخص نشده' : startYear}</h4>
                             </span>
                             <MarkIcon productId={ostadId} />
                         </span>
                         <span className='w-full  flex justify-between items-center gap-2'>
-                            <span className='flex gap-2'>
+                            <span className='flex gap-2 items-center'>
                                 <Clock />
                                 <h4 className='font-medium'>آخرین بروز رسانی: {created_at.toLocal}</h4>
                             </span>
@@ -88,29 +88,32 @@ export default async function Ostad({ params }) {
 
                 <div className='h-fit row-start-1 row-end-4 col-start-1 col-end-4 p-2'>
                     <div className='my-5 md:my-12'>
-                        <span className='flex gap-2'>
+                        <span className='flex gap-2 items-center'>
                             <UserSquare />
-                            <h2 className='text-lg md:text-xl md:font-semibold md:mb-1'>بیوگرافی:</h2>
+                            <h2 className='text-xl font-bold md:mb-1'>بیوگرافی:</h2>
                         </span>
-                        <p className='p-2 text-sm md:text-lg whitespace-pre-line'>{biography || 'توضیحی ثبت نشده'}</p>
+                        <p className='px-4 text-sm md:text-lg whitespace-pre-line'>{biography || 'توضیحی ثبت نشده'}</p>
                     </div>
                     <div className='flex flex-col gap-2 mb-12 justify-between'>
 
-                        <span className='flex gap-2'>
+                        <span className='flex gap-2 items-center'>
                             <BookOpenTextIcon />
-                            <h4 className='font-medium'>استاد {name} این درس ها را ارایه می کند:</h4>
+                            <h3 className='text-lg font-bold'>استاد {name} این درس ها را ارایه می کند:</h3>
                         </span>
-                        {courses.length ? courses.map((date, index) => {
-                            return (
-                                <div key={index} className='flex gap-2'>
-                                    <h4>{date.name}</h4>
-                                    <h4>{date.day}</h4>
-                                    <h4>از ساعت {date.startTime} تا {date.endTime}</h4>
-                                </div>
-                            )
-                        }) :
-                            <h4>درسی برای استاد {name} تعریف نشده</h4>
-                        }
+                        <span className='px-4'>
+                            {courses.length ? courses.map((course, index) => {
+                                return (
+                                    <h4>{course.name} روز {course.day} از ساعت {course.startTime} تا {course.endTime}</h4>
+                                    // <div key={index} className='flex gap-3'>
+                                    //     <h4>{date.name}</h4>
+                                    //     <h4>{date.day}</h4>
+                                    //     <h4>از ساعت {date.startTime} تا {date.endTime}</h4>
+                                    // </div>
+                                )
+                            }) :
+                                <h4>درسی برای استاد {name} تعریف نشده</h4>
+                            }
+                        </span>
                     </div>
 
                     <div className='flex flex-col md:flex-row gap-2 mb-12 justify-between'>
